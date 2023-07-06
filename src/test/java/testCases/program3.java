89433dc93d7a41896af3f8651ef44a2893e1cf01
Soumyajit Sadhu <soumsadh@in.ibm.com>
2023-07-07 03:23:16 +0530

    Fifth Commit

--- src/test/java/testCases/program3.java
+++ src/test/java/testCases/program3.java
@@ -8,6 +8,7 @@ public class program3 {
 		int a  = 12;
 		int b = 4;
 		
+		System.out.println("This is Program3");
 		System.out.println("Multiplication of " + a +" and " + b + " is " + (a*b));
 				
 	}
